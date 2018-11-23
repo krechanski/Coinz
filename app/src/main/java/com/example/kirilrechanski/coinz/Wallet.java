cfb06addcf06d40b3f10e0cd256bb6df8c8a22fd
s1527764 <s1527764@sms.ed.ac.uk>
2018-11-23 15:47:00 +0000

    Added GridView

--- app/src/main/java/com/example/kirilrechanski/coinz/Wallet.java
+++ app/src/main/java/com/example/kirilrechanski/coinz/Wallet.java
@@ -4,7 +4,11 @@ import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.GridView;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.Task;
@@ -28,30 +32,40 @@ public class Wallet extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_wallet);
-        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
-        setSupportActionBar(toolbar);
 
-        mAuth = FirebaseAuth.getInstance();
-        user = mAuth.getCurrentUser();
+        GridView gridview = (GridView) findViewById(R.id.gridview);
+        gridview.setAdapter(new ImageAdapter(this));
 
-        databaseReference = FirebaseFirestore.getInstance();
-        DocumentReference docRef = databaseReference.collection("users").document(user.getUid());
-        docRef.get().addOnCompleteListener(new OnCompleteListener<DocumentSnapshot>() {
-            @Override
-            public void onComplete(@NonNull Task<DocumentSnapshot> task) {
-                coinsCollected = task.getResult().get("coinsCollected").toString();
-                sumCoins = task.getResult().get("sumCoins").toString();
-
-
-                TextView coinsCollectedText = findViewById(R.id.collectedCoins);
-                TextView coinsCollectedValue = findViewById(R.id.collectedCoinsValue);
-                coinsCollectedValue.setText(coinsCollected);
-
-                TextView moneyAvaiableText = findViewById(R.id.moneyAvaiableText);
-                TextView moneyAvaiableValue = findViewById(R.id.moneyAvaiableValue);
-                moneyAvaiableValue.setText(sumCoins);
+        gridview.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            public void onItemClick(AdapterView<?> parent, View v,
+                                    int position, long id) {
+                Toast.makeText(Wallet.this, "" + position,
+                        Toast.LENGTH_SHORT).show();
             }
         });
+
+//        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+//        setSupportActionBar(toolbar);
+//        mAuth = FirebaseAuth.getInstance();
+//        user = mAuth.getCurrentUser();
+//
+//        databaseReference = FirebaseFirestore.getInstance();
+//        DocumentReference docRef = databaseReference.collection("users").document(user.getUid());
+//        docRef.get().addOnCompleteListener(new OnCompleteListener<DocumentSnapshot>() {
+//            @Override
+//            public void onComplete(@NonNull Task<DocumentSnapshot> task) {
+//                coinsCollected = task.getResult().get("coinsCollected").toString();
+//                sumCoins = task.getResult().get("sumCoins").toString();
+//
+//
+//                TextView coinsCollectedValue = findViewById(R.id.collectedCoinsValue);
+//                coinsCollectedValue.setText(coinsCollected);
+//
+//                TextView moneyAvaiableText = findViewById(R.id.moneyAvaiableText);
+//                TextView moneyAvaiableValue = findViewById(R.id.moneyAvaiableValue);
+//                moneyAvaiableValue.setText(sumCoins);
+//            }
+//        });
     }
 
 }
